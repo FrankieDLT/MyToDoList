6bdf3f94b5618f53c2ea251e98de358ae1f29009
FrankieDLT <51407226+FrankieDLT@users.noreply.github.com>
2021-07-14 14:43:49 -0500

    Added prototype of To Do List
    The prototype version of how the list will react has been added. The list will be displayed based on an array of objects and will display the information inside the designated area, the add button now add a testing note to the list.

--- MyTuduList/src/app/app.component.ts
+++ MyTuduList/src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyTuduList';
+
+  realList = [] as  any;
+
+  count = 1;
+
+  toDoList = [
+    {
+      title: "Breathe",
+      description: "I mean, you need it"
+    },
+    {
+      title: "Think",
+      description: "I mean, you have to"
+    },
+    {
+      title: "Carry on",
+      description: "Nothing its permanent"
+    },
+  ]
+
+  
+  addNote(){
+    this.realList.push({
+      title: "Add test: " + this.count++,
+    description: "Testing"})
+    alert("Added");
+  }
+
+
+  ngOnInit(): void{
+    if(!(this.realList.length>0)){
+      this.toDoList.forEach(val => this.realList.push(val));
+    }
+
+    console.log(this.realList)
+  }
+
 }
